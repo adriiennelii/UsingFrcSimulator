3f536dedefbecb53cd7397757d23234ee4d99901
Chris DuPuis <cpdupuis@gmail.com>
2020-10-29 21:13:56 -0700

    Create drive command and subsystem

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,40 +7,42 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
 
-/**
- * An example command that uses an example subsystem.
- */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
+public class DriveCommand extends CommandBase {
+  private final DriveSubsystem driveSubsystem;
+  private final Supplier<Double> leftThrottle;
+  private final Supplier<Double> rightThrottle;
   /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
+   * Creates a new DriveCommand.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public DriveCommand(DriveSubsystem driveSubsystem, Supplier<Double> leftThrottle, Supplier<Double> rightThrottle) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(driveSubsystem);
+    this.driveSubsystem = driveSubsystem;
+    this.leftThrottle = leftThrottle;
+    this.rightThrottle = rightThrottle;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    driveSubsystem.reset();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    driveSubsystem.setThrottle(leftThrottle.get(), rightThrottle.get());
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    driveSubsystem.reset();
   }
 
   // Returns true when the command should end.
